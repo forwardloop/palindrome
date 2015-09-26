1956295e26ea64fc0c28b7bddc8514328cc533ac
kris <kris.lachor@gmail.com>
2015-09-26 19:54:11 +0100

    dev

--- src/main/scala/Palindrome.scala
+++ src/main/scala/Palindrome.scala
@@ -3,26 +3,18 @@ import scala.annotation.tailrec
 object Palindrome {
 
   @tailrec
-  def findPalinsWithLength(s: String, idx: Int, len: Int, acc: List[Palindrome]): List[Palindrome] = {
-    if (idx + len > s.length) acc
+  def findPalinsWithLength(s: String, idx: Int, len: Int, accum: List[Palindrome]): List[Palindrome] = {
+    if (idx + len > s.length) accum
     else {
       val s1 = s.substring(idx, idx + len)
       val acc1: List[Palindrome] =
-        if (isPalin(s1) && isNew(s1, acc)) Palindrome(s1, idx, len) :: acc
-        else acc
+        if (isPalin(s1) && isNew(s1, accum)) Palindrome(s1, idx, len) :: accum
+        else accum
 
       findPalinsWithLength(s, idx + 1, len, acc1)
     }
   }
 
-  @tailrec
-  def findPalins(s: String, len: Int, accum: List[Palindrome], cutOff: Int): List[Palindrome] = {
-    if (len <= 1 || accum.size == 3) accum
-    else {
-      println(s"findPalin $len acc $accum")
-      findPalins(s, len - 2, findPalinsWithLength(s, 0, len, accum), cutOff)
-    }
-  }
 
   def isPalin(s: String): Boolean =
     if (s.length % 2 == 0) {  //odd length strings are not palindromes
@@ -35,11 +27,28 @@ object Palindrome {
 
   def isNew(s: String, acc: List[Palindrome]): Boolean = acc.find(_.txt.contains(s)).isEmpty
 
-  def findLongestPalins(s: String, howMany: Int) = findPalins(s, s.length, Nil, howMany)
 
-  def main(args: Array[String]): Unit = println( findLongestPalins("sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop", 3) )
+  def findLongestPalins(s: String, howMany: Int) = {
+
+    @tailrec
+    def findPalins(s: String, len: Int, accum: List[Palindrome], cutOff: Int): List[Palindrome] =
+      if (len <= 1 || accum.size == cutOff) accum
+      else {
+        println(s"findPalin $len acc $accum")
+        findPalins(s, len - 1, findPalinsWithLength(s, 0, len, accum), cutOff)
+      }
+
+    findPalins (s, s.length, Nil, howMany) sortBy (- _.len)
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    val palins: List[Palindrome] = findLongestPalins("sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop", 3)
+    println(s"\n${palins mkString ("\n\n")}")
+  }
 
 }
+
 
 case class Palindrome(txt: String, idx: Int, len: Int) {
 
