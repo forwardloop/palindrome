b1af581e380e615e37fbb7958c87cde5bc85822b
kris <kris.lachor@gmail.com>
2015-09-26 19:40:03 +0100

    dev

--- src/main/scala/Palindrome.scala
+++ src/main/scala/Palindrome.scala
@@ -3,29 +3,29 @@ import scala.annotation.tailrec
 object Palindrome {
 
   @tailrec
-  def findPalinLen(s: String, idx: Int, len: Int, acc: List[Palindrome]): List[Palindrome] = {
+  def findPalinsWithLength(s: String, idx: Int, len: Int, acc: List[Palindrome]): List[Palindrome] = {
     if (idx + len > s.length) acc
     else {
       val s1 = s.substring(idx, idx + len)
       val acc1: List[Palindrome] =
-        if (isPalindrome(s1) && isNew(s1, acc)) Palindrome(s1, idx, len) :: acc
+        if (isPalin(s1) && isNew(s1, acc)) Palindrome(s1, idx, len) :: acc
         else acc
-      findPalinLen(s, idx + 1, len, acc1)
+
+      findPalinsWithLength(s, idx + 1, len, acc1)
     }
   }
 
   @tailrec
-  def findPalin(s: String, len: Int, acc: List[Palindrome]): List[Palindrome] = {
-    if (len <= 1 || acc.size == 3) acc
+  def findPalins(s: String, len: Int, accum: List[Palindrome], cutOff: Int): List[Palindrome] = {
+    if (len <= 1 || accum.size == 3) accum
     else {
-      println(s"findPalin $len acc $acc")
-      findPalin(s, len - 2, findPalinLen(s, 0, len, acc))
+      println(s"findPalin $len acc $accum")
+      findPalins(s, len - 2, findPalinsWithLength(s, 0, len, accum), cutOff)
     }
   }
 
-  def isPalindrome(s: String): Boolean =
-    if (s.length % 2 == 0) {
-      //odd length strings are not palindromes
+  def isPalin(s: String): Boolean =
+    if (s.length % 2 == 0) {  //odd length strings are not palindromes
       val half = s.length / 2
       val front = s.take(half)
       val back = s.takeRight(half)
@@ -35,11 +35,9 @@ object Palindrome {
 
   def isNew(s: String, acc: List[Palindrome]): Boolean = acc.find(_.txt.contains(s)).isEmpty
 
+  def findLongestPalins(s: String, howMany: Int) = findPalins(s, s.length, Nil, howMany)
 
-
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-  }
+  def main(args: Array[String]): Unit = println( findLongestPalins("sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop", 3) )
 
 }
 
