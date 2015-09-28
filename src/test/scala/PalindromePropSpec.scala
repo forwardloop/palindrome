9ff89b4c92a2961a365aa96f9415c64ca66516f2
kris <kris.lachor@gmail.com>
2015-09-28 10:48:37 +0100

    property based test

--- src/test/scala/PalindromePropSpec.scala
+++ src/test/scala/PalindromePropSpec.scala
@@ -4,11 +4,12 @@ import Palindrome._
 
 object PalindromePropSpec extends Properties("String") {
 
-
   property("isPalindrome") = forAll { (a: String) =>
     isPalindrome(a + (a.reverse)) == true
   }
 
-
+  property("findLongestPalindromes") = forAll { (a: String, b: String, c: String) =>
+    b.isEmpty || findLongestPalindromes(a + b + b.reverse + c).size > 0
+  }
 
 }
